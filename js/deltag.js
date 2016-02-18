4f82044f08fba60f8a1fe3ce631b43955e5fd5d3
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2016-02-18 12:55:31 -0300

    -

--- js/deltag.js
+++ js/deltag.js
@@ -1,24 +1,9 @@
 $('.carousel').carousel('pause');
 
-
-var imagenes = [];
-var nombres = ["Bolsas de congelamiento NL (CryoStore Freezing Bag)", "DMSO USP", "Sistemas de almacenaje"];
-var descripciones = ["Las bolsas de congelamiento Origen Cryostore es una forma segura y económica para crioreservar los componentes sanguíneos.<br>La bolsa Cryostore ha recibido la autorización de comercialización de la FDA,  y se encuentra indicada para la preservación criogénica de sangre y sus componentes, en nitrógeno líquido. Está hecha de película de EVA, que combina la resistencia y flexibilidad necesarias para entregar estabilidad en nitrógeno liquido,con una excelente flexibilidad, y relación impacto/resistencia a la perforación, así como una buena claridad óptica. EVA tiene una baja transmisión de vapor de agua y buenas propiedades de barrera al gas, que ayuda a preservar la integridad de los componentes de la sangre.",
-	"Lorem 2",
-"Lorem 3"];
-
-//Primer elemento
-$(".producto-nombre").text(nombres[0]);
-$(".producto-descripcion").html(descripciones[0]);
-
-
-$(".producto").on("click", function(){
-	$(".producto").removeClass("selected");
-	$(this).addClass("selected");
-
-	var index = Math.floor($(this).index()/2);
-	$(".producto-nombre").text(nombres[index]);
-	$(".producto-descripcion").html(descripciones[index]);
+$(function(){
+	$.post("./news.php", function(response){
+		$(".news").html(response);
+	});
 });
 
 $("#contact-msg-btn").bind("click", function(){
@@ -74,3 +59,4 @@ function validateMsgForm(){
     alert(response.error);
     return response;
 }
+
---
+++ news.php
@@ -0,0 +1,34 @@
+<?php
+require('./class/simple_html_dom.php');
+
+class article{
+	public $title;
+	public $image;
+	public $author;
+	public $date;
+
+	function fillTemplate(){
+		$html = '<div class="article"><img src="'.$this->image.'" alt="article_image"><div class="title">'.$this->title.'</div><span class="author">'.$this->author.'</span><span class="date">'.$this->date.'</span></div>';
+		return $html;
+	}
+}
+
+// Create DOM from URL
+$html = file_get_html('http://www.sciencemag.org/news');
+
+$articles = $html->find('div.view-article-lists-block-6 article.media');
+
+$html = "";
+foreach ($articles as $a){
+	$article = new article();
+	$link = $a->find('div.media__body a',0);
+	$article->title = '<a href="http://www.sciencemag.org'.$link->href.'">'.$link->innertext.'</a>';
+	$article->image = $a->find('div.media__icon img', 0)->src;
+	$article->author = $a->find('div.media__body p a', 0)->innertext;
+	$article->date = $a->find('div.media__body p time', 0)->innertext;
+
+	$html .= $article->fillTemplate();
+}
+
+echo $html;
+?>
