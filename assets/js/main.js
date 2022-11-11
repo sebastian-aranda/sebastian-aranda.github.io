fb9d8ac96559afcb6b684baf086ee8e86eb2bdde
sebastian-aranda <aranda.sebastian@gmail.com>
2022-11-11 19:30:43 -0300

    SAHP v3.2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,12 +58,12 @@
 			$nav_li = $nav.find('li');
 
 		// Add "middle" alignment classes if we're dealing with an even number of items.
-			if ($nav_li.length % 2 == 0) {
+			// if ($nav_li.length % 2 == 0) {
 
-				$nav.addClass('use-middle');
-				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
+			// 	$nav.addClass('use-middle');
+			// 	$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
 
-			}
+			// }
 
 	// Main.
 		var	delay = 325,
